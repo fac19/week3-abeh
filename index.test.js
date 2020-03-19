525e381003634ccddb33e0c0e1d6b1e624a26ae3
Joe Jackson <joe.m.jackson@live.co.uk>
2020-03-19 12:17:52 +0000

    Create integration tests for adding list items Relates #1 Co-authored-by: Tom <wagner.tomek@gmail.com>

--- index.test.js
+++ index.test.js
@@ -1,22 +1,80 @@
-// test("Check that after submit button is clicked a value is in the list", t => {
-//   const result = ["Buy sausages"].length;
-//   t.equal(result, 1);
+function addListItemToDom(list) {
+  list.forEach(inputValueInList => {
+    toDoInput.value = inputValueInList;
+    submit.click();
+    toDoInput.value = "";
+  })  
+}
+// addListItemToDom(['Cheese', 'Ham', 'Fish', 'Potatoes']);
+
+test("Check that first user input correctly renders on the webpage", t => {
+  addListItemToDom(['Walk my cat']);
+  const firstListItemText = document.querySelector('.itemCheckboxLabel').textContent;
+  t.equal(firstListItemText, "Walk my cat");
+  toDoInput.value = "";
+  const firstListItem = document.querySelector('.listContainer__list-item')
+  firstListItem.remove();
+});
+
+test("Check that multiple user inputs correctly render on the webpage", t => {
+  checkArrayLength(['Cheese', 'Ham', 'Fish', 'Potatoes'], 4, t);
+  checkArrayLength(['Cheese', 'Ham', 'Fish', 'Potatoes', 'Fish', 'Potatoes', 'Steak'], 5, t);
+});
+
+function checkArrayLength (array, expectedLength, test) {
+  addListItemToDom(array);
+  let liNodes = document.querySelectorAll('li');
+  const result = liNodes.length;
+  const expected = expectedLength;
+  test.equal(result, expected, `List length expected to be ${expected} and it was ${result}`);
+  liNodes.forEach(node => node.remove());
+}
+
+test("Check that multiple user inputs correctly render on the webpage", t => {
+  addListItemToDom(['Cheese', 'Ham', 'Fish', 'Potatoes']);
+  let liNodes = document.querySelectorAll('li');
+  const result = document.querySelectorAll('li').length;
+  const expected = 4;
+  t.equal(result, expected, `List length expected to be ${expected} and it was ${result}`);
+  liNodes.forEach(node => node.remove());
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Old test function that was limited to only testing one array at a time.
+// test("Check that multiple user inputs correctly render on the webpage", t => {
+//   addListItemToDom(['Cheese', 'Ham', 'Fish', 'Potatoes']);
+//   const liNodes = document.querySelectorAll('li');
+//   const result = liNodes.length;
+//   const expected = 4;
+//   t.equal(result, expected, `List length expected to be ${expected} and it was ${result}`);
+//   liNodes.forEach(node => node.remove());
 // });
 
-// test("check if the todo item text matches user's input", t => {
-//   const result = document.querySelector("#toDo");
-//   const expected = t.equal(result, xx); // targeting a text input of crated todo item
-// });
 
-// test("Check that the input correctly renders on the webpage", t => {
-//   const input = document.querySelector("#toDo");
-//   input.value = "Walk my cat";
-//   const submitButton = document.querySelector("button");
-//   submitButton.click();
-//   // Here we will grab the result from the DOM
-//   // Check that it equals 'Walk my cat'
-//   // Reset the input to ''
-// });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // function checkLengthArr()
 
@@ -28,7 +86,7 @@
   // DOM manipulation li is appended to ul parent
 // });
 
-// // Integration test for calculator
+// Integration test for calculator
 // test("Uppercase feature correctly changes the user's input and updates the page", t => {
 //   const input = document.querySelector("input"); // step 1
 //   input.value = "hello world"; // step 2
