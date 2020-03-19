ca2d20af0b19a7fb0f0c02bd8065b3f8f3b4ba28
tacotoemeck <wagner.tomek@gmail.com>
2020-03-19 15:37:36 +0000

    Add some styling

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-const submit = document.querySelector('#submit');
-const toDoInput = document.querySelector('#toDoInput');
+const submit = document.querySelector("#submit");
+const toDoInput = document.querySelector("#toDoInput");
 const template = document.querySelector("#listItemTemplate");
 const listContainer = document.getElementById("listContainer");
 
-listContainer.addEventListener('click', (event) => {
-    if (event.target.className === 'listContainer__delete-button'){
-        event.target.parentNode.remove()
+listContainer.addEventListener("click", event => {
+  if (event.target.className === "listContainer__delete-button") {
+    event.target.parentNode.remove();
+  }
+  if (event.target.classList.contains("itemCheckboxLabel")) {
+    let label = event.target.parentNode.children[0];
+    let checkbox = event.target.parentNode.children[1];
+    if (checkbox.checked) {
+      checkbox.checked = false;
+      label.classList.remove("checked");
+    } else if (!checkbox.checked) {
+      checkbox.checked = true;
+      label.classList.add("checked");
     }
-    if (event.target.className === 'itemCheckboxLabel'){
-        let label = event.target.parentNode.children[0]
-        let checkbox = event.target.parentNode.children[1]
-        if (checkbox.checked){
-            label.classList.remove('checked')
-            checkbox.checked = false;
-        } else if (!checkbox.checked){
-            label.classList.add('checked')
-            checkbox.checked = true;
-        }
-    }
-})
+  }
+});
 
 submit.addEventListener("click", e => {
-    e.preventDefault();
-    var textAreaContent = toDoInput.value;
-    createListItemUsingTemplate(textAreaContent)
-})
+  e.preventDefault();
+  var textAreaContent = toDoInput.value;
+  createListItemUsingTemplate(textAreaContent);
+});
 
 function createListItemUsingTemplate(textAreaContent) {
-    const docFrag = template.content.cloneNode(true);
-    docFrag.querySelector(".itemCheckboxLabel").textContent = textAreaContent;
-    listContainer.appendChild(docFrag);
+  const docFrag = template.content.cloneNode(true);
+  docFrag.querySelector(".itemCheckboxLabel").textContent = textAreaContent;
+  listContainer.appendChild(docFrag);
 }
