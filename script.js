96e32ff750f81ced56b2200d6e126277b79b3fe9
tacotoemeck <wagner.tomek@gmail.com>
2020-03-19 16:06:14 +0000

    Add h1 heading and font styling Relates #1 Co-authored-by: Joe <joe.m.jackson@live.co.uk>

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ submit.addEventListener("click", e => {
   let textAreaContent = toDoInput.value;
   createListItemUsingTemplate(textAreaContent);
   toDoInput.value = "";
+  // toDoInput.focus();
 });
 
 function createListItemUsingTemplate(textAreaContent) {
